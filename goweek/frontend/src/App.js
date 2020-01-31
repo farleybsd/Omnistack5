a2dac4425ad788ee9dc539117424322bb8cab10d
farleybsd <farley.t.i@hotmail.com>
2020-01-30 21:34:33 -0300

    Primeiras Rotas

---
+++ goweek/frontend/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+
+import Login from './pages/login'
+import Timeline from './pages/TimeLine'
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/TimeLine" component={Timeline} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+
+}
+
+export default App;
+
