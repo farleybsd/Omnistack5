e2d714e9a5fb8b43ac2ad7898d5931de3b010dac
farleybsd <farley.t.i@hotmail.com>
2020-02-04 20:27:00 -0300

    Login

--- goweek/frontend/src/App.js
+++ goweek/frontend/src/App.js
@@ -23,3 +23,4 @@ class App extends Component {
 
 export default App;
 
+// Parei  00:14:52
--- goweek/frontend/src/pages/login.js
+++ goweek/frontend/src/pages/login.js
@@ -1,8 +1,39 @@
-import React ,{Component} from 'react'
-
+import React, { Component } from 'react'
+import TweetLogo from '../twitter.svg'
+import './login.css'
 
 export default class login extends Component {
-    render(){
-        return <h1>Login</h1>
+    state = {
+        username: '',
+    }
+    handleInputChange = (e) =>{
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    handleSubmit = e =>{
+        e.preventDefault()
+
+        const {username} = this.state
+
+        if (!username.length) return
+        localStorage.setItem('@GoTwitter:username',username)
+        this.props.history.push('/timeline')
+    }
+    render() {
+        return (
+            <div className="login-wrapper">
+                <img src={TweetLogo} alt='GoTwittwe' />
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        placeholder="Nome de usuário"
+                    />
+                    <button type="submit">Entar</button>
+                </form>
+            </div>
+        )
     }
 }
